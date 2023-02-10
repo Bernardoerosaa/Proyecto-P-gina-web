dfc44935ea1805308da340b77580bd7995367385
Bernardo Erosa <bernardo.emex@prepafrayangelico.edu.mx>
2023-02-09 19:06:21 -0600

    cambioo

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-let nave=document.querySelector (' .nave');
-let body=document. querySelector ('body');
-document. addEventListener ('mousemove', (e)=›{
+let nave=document.querySelector(' .nave');
+let body=document.querySelector('body');
+document.addEventListener ('mousemove',(e)=>{
     nave.style.left=(e.clientX-40)+"px"
-}) 
+});
